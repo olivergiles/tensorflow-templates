6141785fa886426bdfae99f631e0a73ad1cbc770
Oliver Giles <ogiles@email.com>
2022-03-20 14:10:37 +0000

    added setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
+
+setup(name="tftemplates",
+      version="1.0",
+      description="Template models for tensorflow",
+      packages=find_packages(),
+      install_requires=requirements,
+      test_suite='tests',
+      zip_safe=False)

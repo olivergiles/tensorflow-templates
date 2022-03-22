edd2c82962d7e609759cf7243c03db841b4fb0cc
Oliver Giles <ogiles@email.com>
2022-03-22 14:22:21 +0000

    updated hardcoded EPOCHS

--- tftemplates/basic_models/sequential_api/binary.py
+++ tftemplates/basic_models/sequential_api/binary.py
@@ -20,7 +20,7 @@ def main(test=False):
     X, y = make_moons(n_samples=250, noise=0.2, random_state=42)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
     model = create_model()
-    history = model.fit(X_train, y_train, epochs=5, validation_split=0.3, batch_size=32)
+    history = model.fit(X_train, y_train, epochs=EPOCHS, validation_split=0.3, batch_size=32)
     print(model.evaluate(X_test, y_test))
     return history
 
